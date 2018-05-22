5e2d12dfb9bd42bb11c496320afcd0322bdcf507
bryanhmiller <bryankalos@hotmail.com>
2018-05-21 21:33:20 -0500

    Initial commit

--- PizzaCalculator/PizzaCalculator.Android/MainActivity.cs
+++ PizzaCalculator/PizzaCalculator.Android/MainActivity.cs
@@ -12,6 +12,8 @@ namespace PizzaCalculator.Droid
     [Activity(Label = "PizzaCalculator", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -19,8 +21,20 @@ namespace PizzaCalculator.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            SetContentView(Resource.Layout.Main);
+
+            var peopleEntry = FindViewById<EditText>(Resource.Id.peopleEntry);
+            var calculate = FindViewById<Button>(Resource.Id.caculateButton);
+            var pizzaCount = FindViewById<TextView>(Resource.Id.pizzaCountLabel);
+
+            calculate.Click += delegate
+            {
+                var count = int.Parse(peopleEntry.Text);
+                var pizzas = count / 3;
+                pizzaCount.Text = pizzas.ToString();
+            };
+            //global::Xamarin.Forms.Forms.Init(this, bundle);
+            // LoadApplication(new App());
         }
     }
 }
